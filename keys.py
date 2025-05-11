b35dabdfecc114e094c2cdd07326a279484eb850
Venius24 <WonderRebirth@mail.ru>
2025-05-11 22:39:18 +0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,1 +1,8 @@
+[Telegram]
+api_id = YOUR_API_ID
+api_hash = 
 
+[Channels]
+donor_channel = YOUR_DONOR_CHANNEL
+my_channel = YOUR_CHANNEL
+start_message_id = YOUR_START_MESSAGE #n + 1 (starts from next)
